8b075b7c0bc116669b30db33c3cece6183461961
circlesharp <circlesharp@live.com>
2020-08-01 05:52:12 +0100

    refactory, put front-end to public

--- public/src/main.js
+++ public/src/main.js
@@ -1,13 +1,11 @@
 import Vue from 'vue'
 import App from './App'
-import ColorList from 'colorui/color-list.js'
 
 Vue.config.productionTip = false
-Vue.ColorList = ColorList
 
 App.mpType = 'app'
 
 const app = new Vue({
-    ...App
+  ...App
 })
 app.$mount()
