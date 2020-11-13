20849143b4cb1f8f105e9693bf5ffbf9a3b67577
circlesharp <circlesharp@live.com>
2020-11-13 18:20:50 +0800

    11-13

---
+++ ui-color/pages/test/params.md
@@ -0,0 +1,29 @@
+申请成功 Application approved
+售货机订单(最近30天) Transaction history(Last 30 days)
+
+订单超过12小时，请联系商家 Order more than 12 hours, please contact merchant
+订单超过30天，请联系商家 Order more than 30 days, please contact merchant
+暂无
+取货口 Collection area
+支付时间 Paid time
+
+出货失败 Delivery failed
+等待出货 Delivery pending
+出货成功 Goods delivered
+等待取货 To be collected
+库存不足,等待取货 Sold out, waiting for collection
+
+未申请 No application found
+申请退款 Request a refund
+退款成功 Refunded
+拒绝退款 Refund refused
+
+商品过期 Goods expired
+
+支付成功：PAID
+暂无: N/A
+上拉加载更多: SWIPE UP TO LOADING MORE 
+暂无记录: NO RECORD
+
+this.$t('NotApplicable')
+$t("NotApplicable")
--- ui-color/pages/test/test.vue
+++ ui-color/pages/test/test.vue
@@ -1,145 +1,40 @@
 <template>
   <view class="padding">
-    
-    <Card class="card" title="申请退款信息">
-      <view class="wrap-refund-info">
-        <view class="item" v-for="item in refundInfo" :key="item.title">
-          <view class="title">{{ item.title }}</view>
-          <view class="img" v-if="item.type === 'img'">
-            <image v-if="item.img" :src="item.img" @tap="viewImage(item.img)"></image>
-            <view v-else>未提交图片</view>
-          </view>
-          <view class="content" v-if="item.type === 'content'">{{item.content}}</view>
-        </view>
-      </view>
-    </Card>
-    
-    <Card class="card" title="订单视频信息" 
-      isExtra
-      extraText="+添加商品"
-    >
-      <view class="wrap-video">
-        <video class="video" v-if="!orderDetail.VIResult[0]" :src="orderDetail.VIResult[0].Url"></video>
-        <view v-else>暂无视频</view>
-      </view>
-    </Card>
-    
-    <Card class="card" title="退款审核处理意见">
-      <view class="wrap-produc">
-        <block v-for="(item, idx) in RefundsProducts" :key="index">
-          <List class="list" :item="item" :type="type" @realQtyChange="onRealQtyChange($event, idx)" />
-        </block>
-      </view>
-    </Card>
-    
-    <Card class="card wrap-remark" title="处理意见">
-      <view v-if="type === 0">
-        <textarea
-          v-model="remarks"
-          placeholder-style="color: #d6d6d6;"
-          placeholder="请填写您的处理意见"
-        />
-      </view>
-      <view v-else>
-        {{ remarks || '未填写' }}
-      </view>
-    </Card>
+    <view class="wrap">
+      <view class="item bg-blue">1</view>
+      <view class="item bg-blue">2</view>
+      <view class="item bg-blue">3</view>
+      <view class="item bg-blue">4</view>
+      <view class="item bg-blue">5</view>
+      <view class="item bg-blue">4</view>
+      <view class="item bg-blue">5</view>
+    </view>
   </view>
 </template>
 
 <script>
-import mock from './refund_detail_mock.js';
-import Card from './Card.vue';
-import List from './SuspiciousList.vue';
-
 export default {
-  components: { Card, List },
+  components: {  },
   data() {
     return {
-      orderDetail: mock.orderDetail,
-      RefundsProducts: mock.RefundsProducts,
-      type: 0, // 0: 可修改 ; 1: 不可修改
-      remarks: '',
+      val: '',
     };
   },
-  created() {
-    console.log(mock);
-  },
-  computed: {
-    refundInfo() {
-      const translate = {
-        REFUNDS_REASON_PRODUCT_VI_MORE: '多识别商品',
-        REFUNDS_REASON_PRODUCT_EXPIRED: '商品过期',
-        REFUNDS_REASON_PRODUCT_NOT_MATCH: '识别错商品',
-      };
-      return [
-        { title: '申请原因', type: 'content', content: translate[this.orderDetail.UserRefundsReason] },
-        { title: '联系电话', type: 'content', content: this.orderDetail.UserRefundsMobile },
-        { title: '申请图片', type: 'img', img: this.orderDetail.UserRefundsImageUrl[0] },
-        { title: '申请说明', type: 'content', content: this.orderDetail.UserRefundsRemarks || '未填写说明' },
-      ];
-    }
-  },
   methods: {
-    viewImage(img) {
-      console.log(img);
-    },
-    onRealQtyChange(qty, idx) {
-      this.RefundsProducts[idx].RealQty = qty;
+    onChange(val) {
+      this.val = val;
     }
-  }
+  },
 }
 </script>
 
 <style lang="scss" scoped>
-.card {
-  & + .card {
-    margin-top: 20upx;
-  }
-}
-
-.wrap-refund-info {
+.wrap {
+  display: flex;
+  flex-wrap: wrap;
   .item {
-    display: flex;
-    align-items: center;
-    & + .item {
-      margin-top: 20upx;
-    }
-    .title {
-      color: #999;
-      margin-right: 40upx;
-    }
-    .img {
-      image {
-        width: 80upx;
-        height: 80upx;
-      }
-    }
-  }
-}
-
-.wrap-video {
-  .video {
-    width: 100%;
-    height: 360upx;
-  }
-  .empty-text {
+    flex: 0 0 33.33%;
     text-align: center;
-  }
-}
-
-.wrap-product {
-  .list + .list {
-    margin-top: 50upx;
-  }
-}
-
-.wrap-remark {
-  textarea {
-    padding: 10upx;
-    width: 100%;
-    height: 200upx;
-    border: 2upx solid #EEEEEE;
   }
 }
 </style>
